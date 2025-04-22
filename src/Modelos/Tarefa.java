66792b353c421c4f3bdf94c5e87e39e6a54a4f65
Vinicius Tutya <viniciustutya07@gmail.com>
2025-04-22 10:05:40 -0300

    adicionei o metodo de marcar como concluida uma tarefa

---
+++ src/Modelos/Tarefa.java
@@ -0,0 +1,32 @@
+package Modelos;
+
+import java.time.LocalDate;
+
+public class Tarefa {
+    private String titulo;
+    private String descricao;
+    private LocalDate dataInicio;
+    private boolean concluida;
+
+
+    public Tarefa(String titulo, String descricao, LocalDate dataInicio, boolean concluida) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataInicio = dataInicio;
+        this.concluida = false;
+    }
+    public boolean isConcluida() {
+        return concluida;
+    }
+    public void marcarConcluida(){
+        this.concluida = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa:" + titulo + "\n" +
+                "Descrição: " + descricao + "\n" +
+                "Data de Início: " + dataInicio + "\n" +
+                "Concluida: " + (concluida ? "Sim" : "Não") + "\n";
+    }
+}
---
+++ src/Servico/GerenciadorDeTarefas.java
@@ -0,0 +1,36 @@
+package Servico;
+
+import Modelos.Tarefa;
+
+import java.util.ArrayList;
+
+public class GerenciadorDeTarefas {
+    private ArrayList<Tarefa> listaTarefa = new ArrayList<>();
+
+    public void adicionarTarefa(Tarefa tarefa) {             // metodo de adicionar tarefa na lista
+        listaTarefa.add(tarefa);
+    }
+    public void removerTarefa(Tarefa tarefa) {               // metodo de remover tarefa da lista
+        if (listaTarefa.isEmpty()) {
+            System.out.println("Lista de tarefas vazia.");
+        } else {
+            for (Tarefa t : listaTarefa) {
+                if (t.equals(tarefa)) {
+                    listaTarefa.remove(t);
+                    System.out.println("Tarefa removida com sucesso.");
+                    return;
+                }
+            }
+            }
+        }
+    public void listarTarefas() {
+        if (listaTarefa.isEmpty()) {
+            System.out.println("Lista de tarefas vazia.");
+        } else {
+            for (Tarefa t : listaTarefa) {
+                System.out.println(t);
+            }
+        }
+
+    }
+}
